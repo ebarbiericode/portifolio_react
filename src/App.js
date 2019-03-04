6f9df2e6bc0232b894ee7f563aafca24a8c48f34
Elisandro <elisandro.barbieri@gmail.com>
2019-03-04 08:35:33 -0300

    Defined Class to expand the Bio

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = { displayBio: true }
+  }
   render() {
+    const bio = this.state.displayBio ? (
+      <div>
+        <p>I live in High Wycombe, and code every day.</p>
+        <p>My favorite language is JavaScript, and I think React.js is awesome.</p>
+        <p>Besides coding, I also love music and pizza!</p>
+      </div>
+    ) : null
+
     return (
       <div className="App">
         <header className="App-header">
           <h1>Hello!</h1>
           <p>My name is Elisandro. I'm a software developer.</p>
           <p>I'm always looking forward to working on meaningful projects.</p>
+          {bio}
         </header>
       </div>
     );
